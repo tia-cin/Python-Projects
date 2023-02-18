c8b1eafe7a0b2496c8f5311050fa489f025ec790
Cintia Arce <cintiaayelenarce@gmail.com>
2023-02-18 18:57:13 -0300

    qr-code

---
+++ qr-generator/index.py
@@ -0,0 +1,17 @@
+import qrcode
+
+def generate_qr_code(text):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size = 10,
+        border = 4
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qr-img.png")
+
+text = input("What would be inside QR code: ")
+generate_qr_code(text)
