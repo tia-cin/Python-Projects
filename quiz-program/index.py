e86097d236940c109f46e3fa122f3229f889a97d
Cintia Arce <cintiaayelenarce@gmail.com>
2023-02-18 18:44:33 -0300

    quiz-program

---
+++ quiz-program/index.py
@@ -0,0 +1,346 @@
+quiz = [
+  {
+    "correctAnswer": "Papa",
+    "incorrectAnswers": [
+      "Pear",
+      "Peer",
+      "Peeler"
+    ],
+    "question": "What word is used in the NATO Phonetic Alphabet for the letter P?",
+  },
+  {
+    "correctAnswer": "12 Drummers Drumming",
+    "incorrectAnswers": [
+      "12 Lords a Leaping",
+      "12 Maids a Milking",
+      "12 Swans a Swimming"
+    ],
+    "question": "In the song \"The Twelve Days Of Christmas\", what did my true love give to me on the 12th day?",
+  },
+  {
+    "correctAnswer": "Fort Knox",
+    "incorrectAnswers": [
+      "Fort Box",
+      "Fort Dox",
+      "Fort Stox"
+    ],
+    "question": "Where is most of America's gold located?",
+    
+  },
+  {
+    "correctAnswer": "Lima",
+    "incorrectAnswers": [
+      "Lame",
+      "Loan",
+      "Love"
+    ],
+    "question": "What word is used in the NATO Phonetic Alphabet for the letter L?",
+    
+  },
+  {
+    "correctAnswer": "Cacophony",
+    "incorrectAnswers": [
+      "Cabotage",
+      "Gardyloo",
+      "Halfpace"
+    ],
+    "question": "Which word is defined as 'a harsh, discordant mixture of sounds'?",
+    
+  },
+  {
+    
+    "correctAnswer": "Atmospheric Pressure",
+    "incorrectAnswers": [
+      "Humidity",
+      "Room Temperature",
+      "Salt in sea water"
+    ],
+    "question": "What is a barometer used to measure?",
+    
+  },
+  {
+    "category": "General Knowledge",
+    "id": "6239f8109129041bf3925cc3",
+    "correctAnswer": "Golf",
+    "incorrectAnswers": [
+      "Garden",
+      "Guard",
+      "God"
+    ],
+    "question": "What word is used in the NATO Phonetic Alphabet for the letter G?",
+    "tags": [
+      "general_knowledge"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [],
+    "isNiche": False
+  },
+  {
+    "category": "General Knowledge",
+    "id": "622a1c367cc59eab6f950336",
+    "correctAnswer": "Queen Victoria",
+    "incorrectAnswers": [
+      "James VI",
+      "Elizabeth I",
+      "George II"
+    ],
+    "question": "Who was the first English monarch to have a Christmas tree?",
+    "tags": [
+      "christmas",
+      "uk",
+      "general_knowledge"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [],
+    "isNiche": False
+  },
+  {
+    "category": "General Knowledge",
+    "id": "622a1c3a7cc59eab6f951063",
+    "correctAnswer": "Experience",
+    "incorrectAnswers": [
+      "Expertise",
+      "Extra Power",
+      "Express"
+    ],
+    "question": "What Does The XP In The Operating System 'Windows XP' Actually Stand For?",
+    "tags": [
+      "general_knowledge"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [],
+    "isNiche": False
+  },
+  {
+    "category": "General Knowledge",
+    "id": "622a1c3d7cc59eab6f951b9c",
+    "correctAnswer": "South Pole",
+    "incorrectAnswers": [
+      "The Peak of Mount Everest",
+      "The Moon",
+      "The Mariana Trench"
+    ],
+    "question": "In 1911 Roald Amundsen became the first person to reach where?",
+    "tags": [
+      "people",
+      "firsts",
+      "general_knowledge"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [],
+    "isNiche": False
+  },
+  {
+    "category": "General Knowledge",
+    "id": "622a1c367cc59eab6f950096",
+    "correctAnswer": "10",
+    "incorrectAnswers": [
+      "6",
+      "8",
+      "12"
+    ],
+    "question": "What Is The Value Of The Letter Q In Scrabble",
+    "tags": [
+      "general_knowledge",
+      "games"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [],
+    "isNiche": False
+  },
+  {
+    "category": "General Knowledge",
+    "id": "622a1c357cc59eab6f94fc7d",
+    "correctAnswer": "Gibberish",
+    "incorrectAnswers": [
+      "Gobbledygook",
+      "Blatherskite",
+      "Flummox"
+    ],
+    "question": "Which word is defined as 'unintelligible or meaningless speech'?",
+    "tags": [
+      "words",
+      "general_knowledge"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [],
+    "isNiche": False
+  },
+  {
+    "category": "General Knowledge",
+    "id": "622a1c357cc59eab6f94fc8c",
+    "correctAnswer": "Conjubilant",
+    "incorrectAnswers": [
+      "Jentacular",
+      "Discombobulate",
+      "Whippersnapper"
+    ],
+    "question": "Which word is defined as 'shouting together with joy'?",
+    "tags": [
+      "words",
+      "general_knowledge"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [],
+    "isNiche": False
+  },
+  {
+    "category": "General Knowledge",
+    "id": "622a1c3a7cc59eab6f951074",
+    "correctAnswer": "Xenon",
+    "incorrectAnswers": [
+      "Halogen",
+      "Neon",
+      "Radon"
+    ],
+    "question": "What chemical element gets is name from the greek word meaning 'stranger'?",
+    "tags": [
+      "general_knowledge"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [],
+    "isNiche": False
+  },
+  {
+    "category": "General Knowledge",
+    "id": "622a1c357cc59eab6f94fc8a",
+    "correctAnswer": "Discombobulate",
+    "incorrectAnswers": [
+      "Cacophony",
+      "Dragoman",
+      "Dragoman"
+    ],
+    "question": "Which word is defined as 'to disconcert of confuse'?",
+    "tags": [
+      "words",
+      "general_knowledge"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [],
+    "isNiche": False
+  },
+  {
+    "category": "General Knowledge",
+    "id": "622a1c377cc59eab6f95045c",
+    "correctAnswer": "7 Swans A Swimming",
+    "incorrectAnswers": [
+      "7 Maids a Milking",
+      "7 Lords A Leaping",
+      "7 Drummers Drumming"
+    ],
+    "question": "What did my true love give to me on the \"Seventh\" day of Christmas\"?",
+    "tags": [
+      "general_knowledge"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [],
+    "isNiche": False
+  },
+  {
+    "category": "General Knowledge",
+    "id": "6239f8351bb3145a698af015",
+    "correctAnswer": "Tango",
+    "incorrectAnswers": [
+      "Treat",
+      "Turn",
+      "Tumble"
+    ],
+    "question": "What word is used in the NATO Phonetic Alphabet for the letter T?",
+    "tags": [
+      "general_knowledge"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [],
+    "isNiche": False
+  },
+  {
+    "category": "General Knowledge",
+    "id": "622a1c3a7cc59eab6f951012",
+    "correctAnswer": "Otter",
+    "incorrectAnswers": [
+      "Squirrel",
+      "Turtle",
+      "Bear"
+    ],
+    "question": "What Sort Of Animal Was Tarka?",
+    "tags": [
+      "general_knowledge"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [],
+    "isNiche": False
+  },
+  {
+    "category": "General Knowledge",
+    "id": "622a1c377cc59eab6f95044b",
+    "correctAnswer": "Tax evasion",
+    "incorrectAnswers": [
+      "Murder",
+      "Fraud",
+      "Bribery"
+    ],
+    "question": "Gangster Al Capone, boss of the Chicago underworld, was finally jailed for 11 years for what crime?",
+    "tags": [
+      "crime",
+      "usa",
+      "chicago",
+      "general_knowledge"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [],
+    "isNiche": False
+  },
+  {
+    "category": "General Knowledge",
+    "id": "622a1c3a7cc59eab6f951043",
+    "correctAnswer": "Lesser Lion",
+    "incorrectAnswers": [
+      "Iron Lion",
+      "Ferocious Lion",
+      "Brave Lion"
+    ],
+    "question": "What is the meaning of the name of the constellation Leo Minor ?",
+    "tags": [
+      "astronomy",
+      "names",
+      "general_knowledge"
+    ],
+    "type": "Multiple Choice",
+    "difficulty": "medium",
+    "regions": [],
+    "isNiche": False
+  }
+]
+
+score = 0
+
+
+for question in quiz:
+    print(f"\n Score: {score} \n")
+    print(question["question"])
+    possible_answers = question["incorrectAnswers"]
+    possible_answers.append(question["correctAnswer"])
+    for inx in range(0, len(possible_answers)):
+        print(f"{str(inx)}. {possible_answers[inx]}")
+    
+    ans_input = input("Please choose an answer: ")
+        
+    if possible_answers[int(ans_input)] == question["correctAnswer"]:
+        print("Correct!")
+        score += 1
+        continue
+    else:
+        print(f"\n Wrong, true answer was {question['correctAnswer']}")
