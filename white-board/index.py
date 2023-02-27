280ce1f9c8e262729e083a79fb65e503b66dd73d
Cintia Arce <cintiaayelenarce@gmail.com>
2023-02-27 14:29:10 -0300

    white-board change pencil width func

--- white-board/index.py
+++ white-board/index.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.colorchooser import askcolor
 from tkinter import ttk
+import tkinter
 
 # colors
 white = "#EBEBEB"
@@ -15,6 +16,12 @@ def show_color(new_col):
     global color
 
     color = new_col
+
+def get_curr_val():
+    return "{: .2f}".format(curr_val.get())
+
+def change_slider(ev):
+    val_label.configure(text=get_curr_val())
 
 def colors_palette():
     id = colors.create_rectangle((10, 10,30,30), fill="black")
@@ -61,11 +68,11 @@ def locale_xy(work):
 def add_line(work):
     global curr_x, curr_y
 
-    canvas.create_line((curr_x, curr_y, work.x, work.y), width=2, fill=color)
+    canvas.create_line((curr_x, curr_y, work.x, work.y), width=get_curr_val(), fill=color)
     curr_x, curr_y = work.x, work.y
 
 def main():
-    global colors, canvas, curr_x, curr_y, color
+    global colors, canvas, curr_x, curr_y, color, curr_val, val_label
 
     # program set up
     root = Tk()
@@ -80,11 +87,11 @@ def main():
     color = "black"
 
     # tools bar
-    Canvas(root, bg=gray, width=80, height=570).place(x=20, y=20)
+    Canvas(root, bg=gray, width=80, height=520).place(x=20, y=20)
 
     # eraser
     eraser = Button(root, text="Eraser", bg=gray, command=new_canvas)
-    eraser.place(x=40, y=550)
+    eraser.place(x=40, y=500)
 
     # color palette
     colors = Canvas(root, bg="#fff", width=40, height=340, bd=0)
@@ -97,6 +104,15 @@ def main():
     canvas.place(x=130, y=20)
     canvas.bind("<Button-1>", locale_xy)
     canvas.bind("<B1-Motion>", add_line)
+
+
+    curr_val = tkinter.DoubleVar()
+
+    slider = ttk.Scale(root, from_=0, to=100, orient="horizontal", command=change_slider, variable=curr_val)
+    slider.place(x=20, y=560)
+
+    val_label = ttk.Label(root, text=get_curr_val())
+    val_label.place(x=20, y=585)
 
     root.mainloop()
 
